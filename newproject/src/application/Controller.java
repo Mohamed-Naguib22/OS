7b2ed2c5812865c39b24fb67ad7ad60698840014
yousefHelly <111719905+yousefHelly@users.noreply.github.com>
2022-12-07 22:08:38 +0200

    Added initial files

---
+++ newproject/src/application/Controller.java
@@ -0,0 +1,12 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+public class Controller {
+	@FXML
+	public void close(ActionEvent e) {
+		System.out.println("close");
+		primaryStage.close();
+	}
+
+}
